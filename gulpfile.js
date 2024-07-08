6f46e2fa31011e5ae4704aa8c5debb99029c8afa
sergio <sergiojunior82@gmail.com>
2024-07-08 13:45:07 -0300

    criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
   return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+  return gulp.src('./src/images/**/*', { encoding: false })
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+// Define tarefas que podem ser executadas em paralelo
+exports.default = gulp.parallel(styles, images);
+
+// Define a função de watch
 exports.watch = function() {
-  gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+  // Adiciona tanto styles quanto images ao watch
+  gulp.watch('./src/styles/*.scss', styles); // Corrigido para apenas 'styles' como argumento
+  gulp.watch('./src/images/**/*', images); // Adiciona o watch para imagens
 }
